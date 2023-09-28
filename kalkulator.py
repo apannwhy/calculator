f4c9f000f768dc27c346a3bfe88e91505c47e01d
apannwhy <69232202+apannwhy@users.noreply.github.com>
2023-09-28 20:36:56 +0700

    Add files via upload

--- kalkulator.py
+++ kalkulator.py
@@ -1,21 +1,36 @@
-# menu
-operator = str(input('Pilih operator "penambahan", "pengurangan", "perkalian", atau "pembagian" : '))
-# pemilihan angka-angka
-number1 = int(input("Masukkan angka pertama: "))
-number2 = int(input("Masukkan angka kedua: "))
+# import packages
+import sys
+import time
 
-# operasi penambahan
-if operator == 'penambahan':
-  res = number1 + number2
-# operasi pengurangan
-elif operator == 'pengurangan':
-  res = number1 - number2
-# operasi perkalian
-elif operator == 'perkalian':
-  res = number1 * number2
-# operasi pembagian
-elif operator == 'pembagian':
-  res = number1 / number2
+x = "curut"
+while x == "curut":
+    # pilihan operasi
+    operator = str(
+        input(
+            'Pilih operator "penambahan", "pengurangan", "perkalian", atau "pembagian" : '
+        )
+    )
+    # pemilihan angka-angka
+    number1 = int(input("Masukkan angka pertama: "))
+    number2 = int(input("Masukkan angka kedua: "))
 
-# print hasil operasi
-print(f'{operator} dari {number1} dan {number2} adalah {res}')
+    # operasi penambahan
+    if operator == "penambahan":
+        res = number1 + number2
+    # operasi pengurangan
+    elif operator == "pengurangan":
+        res = number1 - number2
+    # operasi perkalian
+    elif operator == "perkalian":
+        res = number1 * number2
+    # operasi pembagian
+    elif operator == "pembagian":
+        res = number1 / number2
+
+    # print hasil operasi
+    print(f"{operator} dari {number1} dan {number2} adalah {res}")
+    # input pemelihan user keluar
+    keluar = str(input("apakah anda ingin keluar (y/n) => "))
+    if keluar == "y":
+        time.sleep(2)
+        sys.exit()
